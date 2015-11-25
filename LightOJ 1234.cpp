ced89486ffae8e9a141ffd04199d09f3c8673131
MathIsLife <jr_nirjhor@yahoo.com>
2015-11-26 02:17:15 +0600

    Update LightOJ 1234.cpp

--- LightOJ 1234.cpp
+++ LightOJ 1234.cpp
@@ -43,9 +43,10 @@ void Kill (void) {
     scanf("%d", &T);
     for (k = 1; k <= T; k++) {
         scanf("%d", &n);
-        printf("Case %d: ", k);
+        printf("Case %d: ", k); 
+        double bernouli = 1/(2.0 * n) - 1/(12.0 * n * n) + 1.0/(120.0 * n * n * n * n);
         if (n <= MAX) printf("%0.10lf\n", H[n]);
-        else printf("%0.10lf\n", log(n) + gamma + 1/(2.0 * n) - 1/(12.0 * n * n) + 1.0/(120.0 * n * n * n * n));
+        else printf("%0.10lf\n", log(n) + gamma + bernouli);
     }
 }
 
