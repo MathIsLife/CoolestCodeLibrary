0aebb8e0c25b6e073c3833018e15b29da5055231
MathIsLife <jr_nirjhor@yahoo.com>
2016-02-02 15:57:11 +0600

    Update Toph - Act of Random Kindness.cpp

--- Toph - Act of Random Kindness.cpp
+++ Toph - Act of Random Kindness.cpp
@@ -4,6 +4,12 @@
      | \  |   |  |__/     |  |___| |     | |__/
      |  \ |   |  |  \     |  |   | |     | |  \
     _|   \|  _|_ |   \ \__)  |   |  \___/  |   \
+    
+    	Toph - Act of Random Kindness
+    	
+    	Category: Data Structure, Mathematics
+    	
+    	Keys: Union Find, Counting
 
 */
 
@@ -13,7 +19,6 @@ using namespace std;
 
 typedef long long i64;
 typedef unsigned long long ui64;
-typedef pair <long long, long long> pii;
 
 #define checkbit(n, b) ((n >> b) & 1)
 #define gcd __gcd
@@ -27,7 +32,7 @@ typedef pair <long long, long long> pii;
 i64 N, u, v, Ret = 0, R[MAX], P[MAX], F[MAX];
 
 void MakeSet (void) {
-	for (int i = 0; i < MAX; i++) P[i] = i, R[i] = 0, F[i] = 1;
+    for (int i = 0; i < MAX; i++) P[i] = i, R[i] = 0, F[i] = 1;
 }
 
 i64 Find (i64 x) {
@@ -35,26 +40,26 @@ i64 Find (i64 x) {
 }
 
 void Union (i64 x, i64 y) {
-	i64 x_root = Find(x), y_root = Find(y);
-	if (x_root == y_root) return;
+    i64 x_root = Find(x), y_root = Find(y);
+    if (x_root == y_root) return;
+
+    if (R[x_root] < R[y_root]) 
+	P[x_root] = y_root, F[y_root] += F[x_root];
+
+    else if (R[x_root] > R[y_root]) 
+	P[y_root] = x_root, F[x_root] += F[y_root];
 	
-	if (R[x_root] < R[y_root]) 
-		P[x_root] = y_root, F[y_root] += F[x_root];
-	
-	else if (R[x_root] > R[y_root]) 
-		P[y_root] = x_root, F[x_root] += F[y_root];
-	
-	else 
-		P[y_root] = x_root, ++R[x_root], F[x_root] += F[y_root];
+    else 
+	P[y_root] = x_root, ++R[x_root], F[x_root] += F[y_root];
 }
 
 int main () {
-	MakeSet(); scanf ("%lld", &N); 
-	while (N--) {
-		scanf ("%lld %lld", &u, &v);
-		if (Find(u) != Find(v)) Ret += F[Find(u)] * F[Find(v)];
-		Union(u, v); printf ("%lld\n", Ret);
-	}
+    MakeSet(); scanf ("%lld", &N); 
+    while (N--) {
+	scanf ("%lld %lld", &u, &v);
+	if (Find(u) != Find(v)) Ret += F[Find(u)] * F[Find(v)];
+	Union(u, v); printf ("%lld\n", Ret);
+    }
     return 0;
 }
 
