105f98117d068ff363be9f93668ad37909b987eb
MathIsLife <jr_nirjhor@yahoo.com>
2016-02-28 15:20:47 +0600

    Update Codeforces - Number of Ways.cpp

--- Codeforces - Number of Ways.cpp
+++ Codeforces - Number of Ways.cpp
@@ -36,42 +36,42 @@ int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(0);
 	
-	cin >> n;
-	for (i = 1; i <= n; i++)
-		cin >> A[i], 
-		C[i] = C[i - 1] + A[i];
+    cin >> n;
+    for (i = 1; i <= n; i++)
+	cin >> A[i], 
+	C[i] = C[i - 1] + A[i];
 	
-	if (C[n] % 3 || n <= 2) {
-		cout << 0 << endl;
-		return 0;
-	}
+    if (C[n] % 3 || n <= 2) {
+	cout << 0 << endl;
+	return 0;
+    }
 	
-	S = C[n]/3, Count = Ret = 0;
+    S = C[n]/3, Count = Ret = 0;
 	
-	if (S == 0) {
-		for (i = 1; i < n; i++)
-			if (C[i] == 0) ++Count;
+    if (S == 0) {
+	for (i = 1; i < n; i++)
+	    if (C[i] == 0) ++Count;
 			
-		Ret = Count * (Count - 1)/2;
-		cout << Ret << endl;
-		return 0;
-	}
-	
-	for (i = 1; i < n; i++) {
-		if (C[i] == S) V.push_back(0);
-		
-		else if (C[i] == S + S && !V.empty()) 
-			V.push_back(1), ++Count;
-	}
-	
-	S = V.size();
-	
-	for (i = 0; i < S; i++) {
-		if (V[i] == 0) Ret += Count;
-		else --Count;
-	}
-	
+	Ret = Count * (Count - 1)/2;
 	cout << Ret << endl;
+	return 0;
+    }
+	
+    for (i = 1; i < n; i++) {
+	if (C[i] == S) V.push_back(0);
+		
+	else if (C[i] == S + S && !V.empty()) 
+	    V.push_back(1), ++Count;
+    }
+	
+    S = V.size();
+	
+    for (i = 0; i < S; i++) {
+	if (V[i] == 0) Ret += Count;
+	else --Count;
+    }
+	
+    cout << Ret << endl;
 	
     cout.flush(); 
     return 0;
