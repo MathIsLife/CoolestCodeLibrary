2e89ec2d2cf39fc5e6d467bf5852bfcc482037cf
MathIsLife <jr_nirjhor@yahoo.com>
2016-08-01 21:51:35 +0600

    Update TopCoder - BipartiteConstruction.cpp

--- TopCoder - BipartiteConstruction.cpp
+++ TopCoder - BipartiteConstruction.cpp
@@ -7,22 +7,21 @@ class BipartiteConstruction {public: vector <int> construct (int K);};
 const int n = 20;
 
 vector <int> BipartiteConstruction :: construct (int K) {
-	vector <int> edges;
+    vector <int> edges;
     edges.push_back(n);
     
-	for (int i = n - 1; i; --i) {
-		edges.push_back(i * n + i);
-		for (int j = 0; j < 3; ++j) edges.push_back(i * n + i - 1);
-	}
+    for (int i = n - 1; i; --i) {
+	edges.push_back(i * n + i);
+	for (int j = 0; j < 3; ++j) edges.push_back(i * n + i - 1);
+    }
 
-	int node = 0; while (K) {
-		int r = K % 3;
-		K /= 3;
-		for (int i = 0; i < r; ++i) edges.push_back(node);
-		++node;
-	}    
+    int node = 0; while (K) {
+	int r = K % 3;
+	K /= 3;
+	for (int i = 0; i < r; ++i) edges.push_back(node);
+	++node;
+    }    
 
-	return edges;
+    return edges;
 }
-
 
