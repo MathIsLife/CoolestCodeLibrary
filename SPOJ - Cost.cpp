d5913e6e701c6e82a87b95e3f42a08c0598d7a71
MathIsLife <jr_nirjhor@yahoo.com>
2016-09-23 03:21:29 +0600

    Update SPOJ - Cost.cpp

--- SPOJ - Cost.cpp
+++ SPOJ - Cost.cpp
@@ -21,10 +21,10 @@ const int N = 1e5 + 10;
 const int MOD = 1000000000;
 
 struct edge {
-	int u, v; long long w;
-	bool operator < (const edge &e) const {
-		return w < e.w;
-	}
+    int u, v; long long w;
+    bool operator < (const edge &e) const {
+	return w < e.w;
+    }
 };
 
 edge e[N];
@@ -32,33 +32,33 @@ int n, m, p[N];
 long long sum[N], size[N];
 
 int find (int x) {
-	return p[x] == x ? x : p[x] = find(p[x]);
+    return p[x] == x ? x : p[x] = find(p[x]);
 }
 
 long long merge (int u, int v) {
-	int x = find(u), y = find(v);
-	if (x == y) return 0;
+    int x = find(u), y = find(v);
+    if (x == y) return 0;
 
-	long long ret = (size[x] * size[y]) % MOD;
-	p[x] = y, size[y] += size[x];
+    long long ret = (size[x] * size[y]) % MOD;
+    p[x] = y, size[y] += size[x];
 	
-	return ret;
+    return ret;
 }
 
 int main (int argc, char const *argv[]) {
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= m; ++i) 
-		scanf("%d %d %lld", &e[i].u, &e[i].v, &e[i].w);
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; ++i) 
+	scanf("%d %d %lld", &e[i].u, &e[i].v, &e[i].w);
 
-	sort(e + 1, e + m + 1);
-	for (int i = 1; i <= m; ++i) sum[i] = (sum[i - 1] + e[i].w) % MOD;
+    sort(e + 1, e + m + 1);
+    for (int i = 1; i <= m; ++i) sum[i] = (sum[i - 1] + e[i].w) % MOD;
 
-	for (int i = 1; i <= n; ++i) p[i] = i, size[i] = 1;
+    for (int i = 1; i <= n; ++i) p[i] = i, size[i] = 1;
 
-	long long res = 0;
-	for (int i = m; i; --i) res += merge(e[i].u, e[i].v) * sum[i], res %= MOD;
+    long long res = 0;
+    for (int i = m; i; --i) res += merge(e[i].u, e[i].v) * sum[i], res %= MOD;
 
-	printf("%lld\n", res);
-	return 0;
+    printf("%lld\n", res);
+    return 0;
 }
 
