54a181da265f31e35de58c843d7ed6fd856bcf79
MathIsLife <jr_nirjhor@yahoo.com>
2017-03-04 00:25:08 +0600

    Update NCPC 2015 - Farey Sequence.cpp

--- NCPC 2015 - Farey Sequence.cpp
+++ NCPC 2015 - Farey Sequence.cpp
@@ -23,28 +23,28 @@ int t, n;
 long long phiSum[N];
 
 void generate_totient (void) {
-	for (int i = 2; i < N; ++i) {
-		phiSum[i] = i;
+    for (int i = 2; i < N; ++i) {
+	phiSum[i] = i;
+    }
+    for (int i = 2; i < N; ++i) {
+	if (phiSum[i] == i) {
+	    for (int j = i; j < N; j += i) {
+		phiSum[j] /= i;
+		phiSum[j] *= (i - 1);
+	    }
 	}
-	for (int i = 2; i < N; ++i) {
-		if (phiSum[i] == i) {
-			for (int j = i; j < N; j += i) {
-				phiSum[j] /= i;
-				phiSum[j] *= (i - 1);
-			}
-		}
-	}
-	for (int i = 1; i < N; ++i) {
-		if (phiSum[i] == i) --phiSum[i];
-		phiSum[i] += phiSum[i - 1];
-	}
+    }
+    for (int i = 1; i < N; ++i) {
+	if (phiSum[i] == i) --phiSum[i];
+	phiSum[i] += phiSum[i - 1];
+    }
 }
 
 int main (int argc, char const *argv[]) {
-	generate_totient();
-	while (scanf("%d", &n) and n) {
+    generate_totient();
+    while (scanf("%d", &n) and n) {
     	printf("%lld\n", phiSum[n]);
-	}
-	return 0;
+    }
+    return 0;
 }
 
