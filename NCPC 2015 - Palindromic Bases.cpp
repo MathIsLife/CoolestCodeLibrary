a891d695ca6fd830982fe5d5275f5f82fd540577
MathIsLife <jr_nirjhor@yahoo.com>
2017-03-03 23:55:49 +0600

    Update NCPC 2015 - Palindromic Bases.cpp

--- NCPC 2015 - Palindromic Bases.cpp
+++ NCPC 2015 - Palindromic Bases.cpp
@@ -31,50 +31,50 @@ vector <ll> primes;
 vector <pii> factors;
 
 void sieve (void) {
-	for (int i = 2; i * i < N; ++i) if (!vis[i]) {
-		for (int j = i * i; j < N; j += i) vis[j] = 1;
-	}
-	for (int i = 2; i < N; ++i) {
-		if (!vis[i]) primes.push_back((ll) i);
-	}
-	size = primes.size();
+    for (int i = 2; i * i < N; ++i) if (!vis[i]) {
+	for (int j = i * i; j < N; j += i) vis[j] = 1;
+    }
+    for (int i = 2; i < N; ++i) {
+	if (!vis[i]) primes.push_back((ll) i);
+    }
+    size = primes.size();
 }
 
 bool check (ll x, ll base) {
-	if (base < 2) return false;
-	int l = 0;
-	while (x) {
-		tmp[++l] = x % base;
-		x /= base;
-	}
-	if (l & 1) return false;
-	for (int i = 1, j = l; i < j; ++i, --j) {
-		if (tmp[i] != tmp[j]) return false;
-	}
-	return true;
+    if (base < 2) return false;
+    int l = 0;
+    while (x) {
+	tmp[++l] = x % base;
+	x /= base;
+    }
+    if (l & 1) return false;
+    for (int i = 1, j = l; i < j; ++i, --j) {
+	if (tmp[i] != tmp[j]) return false;
+    }
+    return true;
 }
 
 void backtrack (int pos, ll d) {
-	if (pos == tot) {
-		if (check(m, d - 1)) ++res;
-		return;
-	}
-	for (int j = 0; j <= factors[pos].second; ++j) {
-		backtrack(pos + 1, d);
-		d *= factors[pos].first;
-	}
+    if (pos == tot) {
+	if (check(m, d - 1)) ++res;
+	return;
+    }
+    for (int j = 0; j <= factors[pos].second; ++j) {
+	backtrack(pos + 1, d);
+	d *= factors[pos].first;
+    }
 }
 
 int main (int argc, char const *argv[]) {
-	sieve();
+    sieve();
     scanf("%d", &t); while (t--) {
     	scanf("%lld", &n);
     	m = n, res = 0;
     	for (int i = 0; i < size; ++i) {
-    		if (primes[i] * primes[i] > n) break;
-    		int e = 0;
-    		while (n % primes[i] == 0) ++e, n /= primes[i];
-    		factors.push_back(pii(primes[i], e)); 
+    	    if (primes[i] * primes[i] > n) break;
+    	    int e = 0;
+    	    while (n % primes[i] == 0) ++e, n /= primes[i];
+    	    factors.push_back(pii(primes[i], e)); 
     	}
     	if (n > 1) factors.push_back(pii(n, 1));
     	tot = factors.size();
