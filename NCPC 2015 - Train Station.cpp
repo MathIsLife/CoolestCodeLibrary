4b03ddde07f635c992794893df8453da9ecd0c17
MathIsLife <jr_nirjhor@yahoo.com>
2017-03-04 00:57:08 +0600

    Update NCPC 2015 - Train Station.cpp

--- NCPC 2015 - Train Station.cpp
+++ NCPC 2015 - Train Station.cpp
@@ -24,10 +24,10 @@ int t, cs = 0;
 long double x, a, b, d;
 
 long double at (long double p) {
-	long double one = hypot(p, x);
-	long double two = hypot(p + d, x);
-	long double ret = one * two + d * (one + two) - one * one - p * d;
-	return ret/2.0;
+    long double one = hypot(p, x);
+    long double two = hypot(p + d, x);
+    long double ret = one * two + d * (one + two) - one * one - p * d;
+    return ret/2.0;
 }
 
 int main (int argc, char const *argv[]) {
@@ -36,14 +36,14 @@ int main (int argc, char const *argv[]) {
     	a -= d;
     	printf("Case %d: ", ++cs);
     	if (fabs(a - b) < EPS) {
-    		long double ans = at(b)/d;
-    		printf("%0.12f\n", (double) ans);
-    		continue;
+    	    long double ans = at(b)/d;
+    	    printf("%0.12f\n", (double) ans);
+    	    continue;
     	}
     	long double jump = (a - b)/MAGIC;
     	long double ans = (at(a) + at(b))/2.0;
     	for (int k = 1; k < MAGIC; ++k) {
-    		ans += at(b + k * jump);
+    	    ans += at(b + k * jump);
     	}
     	ans *= jump, ans /= d, ans /= (a - b);
     	printf("%0.12f\n", (double) ans);
