670c4920133c9d17435e6c40cefa49d9beedd737
MathIsLife <jr_nirjhor@yahoo.com>
2017-05-19 18:54:36 +0600

    Update IOI 2016 - Detecting Molecules.cpp

--- IOI 2016 - Detecting Molecules.cpp
+++ IOI 2016 - Detecting Molecules.cpp
@@ -25,22 +25,22 @@ vector <int> find_subset (int l, int u, vector <int> w) {
   int n = w.size();
   vector <pii> v;
   for (int i = 0; i < n; ++i) {
-  	v.push_back(pii(w[i], i));
+    v.push_back(pii(w[i], i));
   }
   sort(v.begin(), v.end());
   ll sum = 0;
   vector <int> r;
   for (int i = 0, j = 0; j < n; ++j) {
-		sum += v[j].first;
-  	while (sum > u) {
-  		sum -= v[i++].first;
-  	}
-  	if (sum >= l) {
-			for (int k = i; k <= j; ++k) {
-				r.push_back(v[k].second);
-			}
-			break;
-		}
+    sum += v[j].first;
+    while (sum > u) {
+      sum -= v[i++].first;
+    }
+    if (sum >= l) {
+      for (int k = i; k <= j; ++k) {
+	r.push_back(v[k].second);
+      }
+      break;
+    }
   }
   return r;
 }
