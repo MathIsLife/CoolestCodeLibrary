4da46276ac6f17da9c2f72308cc04ffe39aa8479
MathIsLife <jr_nirjhor@yahoo.com>
2017-05-26 20:41:05 +0600

    Update IOI 2015 - Boxes with souvenirs.cpp

--- IOI 2015 - Boxes with souvenirs.cpp
+++ IOI 2015 - Boxes with souvenirs.cpp
@@ -24,29 +24,29 @@ vector <long long> costRight;
 long long delivery (int N, int K, int L, int p[]) {
   int mid = L >> 1;
   for (int i = 0; i < N; ++i) {
-  	if (p[i] > mid) {
-  		costRight.push_back(2LL * (L - p[i]));
-  	} else {
-  		costLeft.push_back(2LL * p[i]);
-  	}
+    if (p[i] > mid) {
+      costRight.push_back(2LL * (L - p[i]));
+    } else {
+      costLeft.push_back(2LL * p[i]);
+    }
   }
   reverse(costRight.begin(), costRight.end());
   int sizeLeft = (int) costLeft.size();
   int sizeRight = (int) costRight.size();
   for (int i = K; i < sizeLeft; ++i) {
-  	costLeft[i] += costLeft[i - K];
+    costLeft[i] += costLeft[i - K];
   }
   for (int i = K; i < sizeRight; ++i) {
-  	costRight[i] += costRight[i - K];
+    costRight[i] += costRight[i - K];
   }
   long long leftCost = sizeLeft ? costLeft[sizeLeft - 1] : 0;
   long long rightCost = sizeRight ? costRight[sizeRight - 1] : 0;
   long long ret = leftCost + rightCost;
   for (int i = 0; i <= K; ++i) {
-  	int leftTaken = i, rightTaken = K - i;
-  	long long left = leftTaken >= sizeLeft ? 0 : costLeft[sizeLeft - 1 - leftTaken];
-  	long long right = rightTaken >= sizeRight ? 0 : costRight[sizeRight - 1 - rightTaken];
-  	ret = min(ret, L + left + right);
+    int leftTaken = i, rightTaken = K - i;
+    long long left = leftTaken >= sizeLeft ? 0 : costLeft[sizeLeft - 1 - leftTaken];
+    long long right = rightTaken >= sizeRight ? 0 : costRight[sizeRight - 1 - rightTaken];
+    ret = min(ret, L + left + right);
   }
   return ret;
 }
